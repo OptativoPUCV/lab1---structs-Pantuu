f302813d5ef3861227cb13eda4f6376f531f8241
Your Name <you@example.com>
2024-03-14 18:41:58 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -33,9 +33,9 @@ void reverseArray(int arr[], int size)
   int temp = 0;
   for (int i = 0 ; i < size ; i++)
     {
-      temp = arr[(size - i)];
-      arr[size - i] = arr[i];
-      arr[i] = temp;
+      temp = arr[i];
+      arr[i] = arr[size - i - 1];
+      arr[size - i - 1] = temp;
     }
   
 }
